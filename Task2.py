eeb79e3338760d097d32d31158976e24a46be145
S.M. Nurnobi <34552314+Nurnobi101@users.noreply.github.com>
2020-01-31 18:57:20 +0600

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,39 @@
+import tkinter
+from tkinter import *
+from os import*
+
+def sleepclick():
+   print(system("shutdown /h"))
+def shutdownclick():
+   print(system("shutdown /s /t 0"))
+def restartclick():
+   print(system("shutdown /r /t 0"))
+
+root = tkinter.Tk()
+root.geometry("400x300")
+
+photo = PhotoImage(file="sleep.png")
+photo2 = PhotoImage(file="shutdown.png")
+photo3 = PhotoImage(file="restart.png")
+btn = Button(
+    root,
+    image=photo,
+    command=sleepclick,
+    border=0,
+)
+btn.pack(pady=30)
+btn2 = Button(
+    root,
+    image=photo2,
+    command=shutdownclick,
+    border=0,
+)
+btn2.pack()
+btn3 = Button(
+    root,
+    image=photo3,
+    command=restartclick,
+    border=0,
+)
+btn3.pack( pady=30)
+root.mainloop()
